ab805bae968acc3ed59c322f984d2be7d9d52e97
Phat <dvphat92@gmail.com>
2018-09-13 16:38:18 +0700

    Add find & delete

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient =  require('mongodb').MongoClient;
+// const MongoClient =  require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 const url = 'mongodb://localhost:27017/TodoApp';
 
@@ -15,6 +16,18 @@ MongoClient.connect(url, (err, client) => {
     }, (err, result) => {
         if (err) {
             return console.log('Unable to insert todo', err);
+        }
+
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    db.collection('Users').insertOne({
+        name: 'Phat',
+        age: 26,
+        location: 'VN'
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert user', err);
         }
 
         console.log(JSON.stringify(result.ops, undefined, 2));
