07eb642777c841f9aaf29ca7e09f61286b317e4e
Phat <dvphat92@gmail.com>
2018-09-21 15:27:06 +0700

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -17,12 +17,16 @@ app.post('/todos', (req, res) => {
     todo.save().then((doc) => {
         res.send(doc);
     }, (e) => {
-        res.status(400).send({"errors": e.errors.text.message})
+        res.status(400).send({"errors": e.errors.text.message});
     });
 });
 
 app.get('/todos', (req, res) => {
-
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send({"errors": e.errors.text.message});
+    });
 });
 
 app.listen(3000, () => {
