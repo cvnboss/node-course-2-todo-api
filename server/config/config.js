25c7b53eb4d082d68bad5ebbf23dd6fb5dde2379
Phat <dvphat92@gmail.com>
2018-09-25 13:49:59 +0700

    Uodate config

--- server/config/config.js
+++ server/config/config.js
@@ -3,8 +3,8 @@ console.log(`environments: ${env}`);
 
 if (env = 'development') {
     process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+    process.env.MONGODB_URL = 'mongodb://localhost:27017/TodoApp';
 } else if (env = 'test') {
     process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+    process.env.MONGODB_URL = 'mongodb://localhost:27017/TodoAppTest';
 }
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,6 +5,6 @@ mongoose.set('useFindAndModify', false);
 // const url = 'mongodb://cvnboss:conga012@ds213053.mlab.com:13053/node-todos';
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true});
 
 module.exports = {mongoose};
