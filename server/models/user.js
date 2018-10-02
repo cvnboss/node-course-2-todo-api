294155b26547ee466974d9dc683a0a54d7d446d2
Phat <dvphat92@gmail.com>
2018-10-02 11:36:33 +0700

    Update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -46,7 +46,7 @@ userSchema.methods.generateAuthToken = function() {
     var user = this;
     var access = 'auth';
     var token = jwt
-        .sign({ _id: user._id.toHexString(), access }, 'abc123')
+        .sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET)
         .toString();
 
     // user.tokens.push({access, token});
@@ -72,7 +72,7 @@ userSchema.statics.findByToken = function(token) {
     var decoded;
 
     try {
-        decoded = jwt.verify(token, 'abc123');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (e) {
         return Promise.reject();
     }
--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -15,7 +15,10 @@ const users = [
             {
                 access: 'auth',
                 token: jwt
-                    .sign({ _id: userOneId, access: 'auth' }, 'abc123')
+                    .sign(
+                        { _id: userOneId, access: 'auth' },
+                        process.env.JWT_SECRET
+                    )
                     .toString()
             }
         ]
@@ -28,7 +31,10 @@ const users = [
             {
                 access: 'auth',
                 token: jwt
-                    .sign({ _id: userTwoId, access: 'auth' }, 'abc123')
+                    .sign(
+                        { _id: userTwoId, access: 'auth' },
+                        process.env.JWT_SECRET
+                    )
                     .toString()
             }
         ]
